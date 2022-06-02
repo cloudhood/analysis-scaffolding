050c413641e805239d9964f58e8a20f37e1649d5
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-06-02 16:44:37 +0100

    Update utils.sh

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 function init_venv {
   conda deactivate  # activated by default in Sagemaker Studio Lab
   python3 -m venv $1
-  echo "Appending path to venv to .bashrc..."
-  echo "source $1/bin/activate" >> $HOME/.bashrc
+  # echo "Appending path to venv to .bashrc..."
+  # echo "source $1/bin/activate" >> $HOME/.bashrc
   
   echo "Appending to .gitignore..."
   echo "source $1/bin/activate" >> .gitignore
@@ -13,7 +14,14 @@ function init_venv {
 }
 
 function install_pip {
-  source $1/bin/activate
   pip install --upgrade pip
   pip install -r requirements.txt
+}
+
+function add_ll {
+    echo "Appending ll to .bashrc..."
+    echo "#Alias for listing directory" >> $HOME/.bashrc
+    echo "alias ll='ls -alF --color=auto'" >> $HOME/.bashrc
+    echo "" >> $HOME/.bashrc
+    source $HOME/.bashrc
 }
